0a1902ea4a1a19207b7e53906b8a155e64ad47e8
redwac <sabbane.reda@gmail.com>
2021-06-01 14:44:40 +0000

    application user and password

--- src/main/java/com/example/demo/security/ApplactionSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplactionSecurityConfig.java
@@ -1,13 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplactionSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder ;
+
+    @Autowired
+    public ApplactionSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,5 +33,21 @@ public class ApplactionSecurityConfig extends WebSecurityConfigurerAdapter {
               .authenticated()
               .and()
               .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails redasab = User.builder()
+                .username("redasab")
+                .password(passwordEncoder.encode("pass"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                redasab
+        );
+
     }
 }
